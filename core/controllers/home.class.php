2cdf94db8333e61f0cf2ee6fa1dfb3962393b587
bezumkin <bezumkin@yandex.ru>
2024-03-22 18:33:37 +0700

    Use mmx/database and latest vesp/core

--- core/controllers/home.class.php
+++ core/controllers/home.class.php
@@ -4,28 +4,28 @@ class StercCspHomeManagerController extends \MODX\Revolution\modExtraManagerCont
 {
     public function loadCustomCssJs(): void
     {
-        $server = '127.0.0.1';
-        $port = getenv('NODE_DEV_PORT') ?: '9090';
-        $connection = @fsockopen($server, $port);
         $baseUrl = MODX_ASSETS_URL . 'components/sterc-csp/';
-        if (is_resource($connection)) {
-            // Development mode
-            $path = $server . ':' . $port . $baseUrl;
-            $this->addHtml('<script type="module" src="//' . $path . '@vite/client"></script>');
-            $this->addHtml('<script type="module" src="//' . $path . 'src/main.ts"></script>');
-        } else {
+        $manifest = MODX_ASSETS_PATH . 'components/sterc-csp/manifest.json';
+        if (file_exists($manifest) && $files = json_decode(file_get_contents($manifest), true)) {
             // Production mode
-            $manifest = MODX_ASSETS_PATH . 'components/sterc-csp/manifest.json';
-            if (file_exists($manifest) && $files = json_decode(file_get_contents($manifest), true)) {
-                foreach ($files as $file) {
-                    if (preg_match('#\.js$#', $file['file'])) {
-                        if (!empty($file['isEntry'])) {
-                            $this->addHtml('<script type="module" src="' . $baseUrl . $file['file'] . '"></script>');
-                        }
-                    } elseif (preg_match('#\.css$#', $file['file'])) {
-                        $this->addCss($baseUrl . $file['file']);
+            foreach ($files as $file) {
+                if (preg_match('#\.js$#', $file['file'])) {
+                    if (!empty($file['isEntry'])) {
+                        $this->addHtml('<script type="module" src="' . $baseUrl . $file['file'] . '"></script>');
                     }
+                } elseif (preg_match('#\.css$#', $file['file'])) {
+                    $this->addCss($baseUrl . $file['file']);
                 }
+            }
+        } else {
+            // Development mode
+            $server = '127.0.0.1';
+            $port = getenv('NODE_DEV_PORT') ?: '9090';
+            $connection = @fsockopen($server, $port);
+            if (is_resource($connection)) {
+                $path = $server . ':' . $port . $baseUrl;
+                $this->addHtml('<script type="module" src="//' . $path . '@vite/client"></script>');
+                $this->addHtml('<script type="module" src="//' . $path . 'src/main.ts"></script>');
             }
         }
     }
--- core/src/Console/Console.php
+++ core/src/Console/Console.php
@@ -2,19 +2,44 @@
 
 namespace Sterc\CSP\Console;
 
+use MODX\Revolution\modX;
+use Sterc\CSP\App;
 use Sterc\CSP\Console\Command\Install;
 use Sterc\CSP\Console\Command\Remove;
 use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Command\ListCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class Console extends Application
 {
-    public function __construct()
+    protected modX $modx;
+    protected App $app;
+
+    public function __construct(modX $modx)
     {
         parent::__construct('Sterc CSP');
+        $this->modx = $modx;
+        $this->app = new App($modx);
+    }
 
-        $this->addCommands([
-            new Install(),
-            new Remove(),
-        ]);
+    public function doRun(InputInterface $input, OutputInterface $output)
+    {
+        if (!$this->modx->services->has('mmxDatabase')) {
+            $output->writeln('<error>Could not load mmxDatabase service</error>');
+            $output->writeln('<info>Please run "composer exec mmx-database install"</info>');
+            exit;
+        }
+
+        return parent::doRun($input, $output);
+    }
+
+    protected function getDefaultCommands(): array
+    {
+        return [
+            new ListCommand(),
+            new Install($this->modx),
+            new Remove($this->modx),
+        ];
     }
 }
